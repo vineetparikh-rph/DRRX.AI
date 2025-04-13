89407192bcbe26809e38b57ea40758807364db64
Vineet Parikh <drrxllc@gmail.com>
2025-04-13 14:50:17 -0400

    Fix Tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
